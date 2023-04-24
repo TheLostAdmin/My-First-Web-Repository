3301eda0e7d2b40ba387142483e8fae444549a23
TheLostAdmin <131698510+TheLostAdmin@users.noreply.github.com>
2023-04-24 14:32:16 -0400

    Add files via upload

---
+++ halloween.js
@@ -0,0 +1,11 @@
+
+	
+$("#banner_image img").attr("src","xmas-balls.jpg");
+
+$(".content_image img").attr("src","xmas-1.jpg");
+
+$("#banner_image").click(function(){
+    //$("#banner_image img").fadeToggle();
+	//$("#banner_image img").fadeTo("slow", 0.2)
+});
+	
